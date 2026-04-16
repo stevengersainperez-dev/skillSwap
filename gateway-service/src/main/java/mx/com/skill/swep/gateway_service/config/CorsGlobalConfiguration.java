d3f0a9cef04c517cf0bba15712324fc5e2f08341
Steven Dev <stevengersainperez@gmail.com>
2026-04-16 10:38:24 -0600

    Setting POM

--- gateway-service/src/main/java/mx/com/skill/swep/gateway_service/config/CorsGlobalConfiguration.java
+++ gateway-service/src/main/java/mx/com/skill/swep/gateway_service/config/CorsGlobalConfiguration.java
@@ -1,5 +1,6 @@
 package mx.com.skill.swep.gateway_service.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
